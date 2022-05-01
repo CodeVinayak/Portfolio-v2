46e7ab8361f9d245af9b841833e136b20010afc4
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-05-01 17:34:27 +0530

    checking visit site

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -16,9 +16,12 @@ export function Portfolio(){
           <header>
           <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
           <div className="project-links">
-            <a href="https://github.com/joaotuliojt/fc-squad43" target="_blank" rel="noreferrer">
+            {/* <a href="https://github.com/" target="_blank" rel="noreferrer">
               <img src={githubIcon} alt="GitHub" />
-            </a>
+            </a> */}
+            <a href="https://madewithluv.in/" target="_blank" rel="noreferrer">
+                <img src={externalLinkIcon} alt="Visitar site" />
+              </a>
           </div>
           </header>
           <div className="body">
