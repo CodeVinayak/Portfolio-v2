025336e2042a323e0a0949b1514ca80ca2b3b96f
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-05-10 14:43:02 +0530

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -93,6 +93,9 @@ export function Portfolio() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
+                <a href="https://github.com/CodeVinayak/Portfolio-v1" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visitar site" />
+                </a>
                 <a href="https://portfoliov1.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
                 </a>
@@ -129,7 +132,7 @@ export function Portfolio() {
             <div className="body">
               <h3>Blog</h3>
               <p>
-              Blog about Best Online Shopping Deals and Offers. Enjoy Savings , Add Bookmark our website. Never Miss any Loot Deals and Tricks.
+                Blog about Best Online Shopping Deals and Offers. Enjoy Savings , Add Bookmark our website. Never Miss any Loot Deals and Tricks.
               </p>
             </div>
             <footer>
