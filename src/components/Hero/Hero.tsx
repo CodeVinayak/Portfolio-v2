104f3f83133b1e4c23613d7d25e39804b11ff576
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-05-28 23:58:35 +0530

    contact button updated
    contact button updated in main page

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -22,7 +22,7 @@ export function Hero(){
 
           <ScrollAnimation animateIn="fadeInUp" delay={0.8*1000}>
             <BrowserRouter>
-              <NavHashLink smooth to="#contato" className="button">Contact</NavHashLink>
+              <NavHashLink smooth to="#contact" className="button">Contact</NavHashLink>
             </BrowserRouter>
           </ScrollAnimation>
       </div>
