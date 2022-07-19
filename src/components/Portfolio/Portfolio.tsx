bcaac3d67e6b8e528d93bebe47b84272bd24d384
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-07-19 16:24:51 +0530

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -18,7 +18,7 @@ export function Portfolio() {
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
                 <a href="https://ecommerce.vinayaksingh.in/wp-content/uploads/2022/07/Ecommerce.apk" target="_blank" rel="noreferrer">
-              <img src={DownloadIcon} alt="Download App" />
+              <img src={externalLinkIcon} alt="Download App" />
             </a>
                 <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
@@ -33,7 +33,6 @@ export function Portfolio() {
             <footer>
               <ul className="tech-list">
                 <li>Wordpress</li>
-                <li>WooCommerce</li>
               </ul>
             </footer>
           </div>
@@ -154,22 +153,22 @@ export function Portfolio() {
                 {/* <a href="https://github.com/" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="Visitar site" />
               </a> */}
-                <a href="https://spotify.vinayaksingh.in/" target="_blank" rel="noreferrer">
+                <a href="https://music.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>Spotify Music Library</h3>
+              <h3>Music Library</h3>
               <p>
-              A modern Web-Based music player made using ReactJS. Clean UI with dark mode, High-quality online music streaming with no lag, Ad Free, Download music to your system, Global Music.
+                An simple and clean interface for a short music library.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>React</li>
-                <li>React Redux</li>
-                <li>Redux</li>
+                <li>Html</li>
+                <li>Css</li>
+                <li>JavaSript</li>
               </ul>
             </footer>
           </div>
