98e7852f2fb779ea40d4eec99d562a38ab8fb1ef
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-07-19 16:18:13 +0530

    app download link updated

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,5 +1,6 @@
 import { Container } from "./styles";
 import githubIcon from "../../assets/github-icon.svg"
+import DownloadIcon from "src\assets\download-app.svg"
 import externalLinkIcon from "../../assets/external-link-icon.svg"
 import ScrollAnimation from "react-animate-on-scroll";
 
@@ -16,17 +17,17 @@ export function Portfolio() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                {/* <a href="https://github.com/" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
-            </a> */}
+                <a href="https://ecommerce.vinayaksingh.in/wp-content/uploads/2022/07/Ecommerce.apk" target="_blank" rel="noreferrer">
+              <img src={DownloadIcon} alt="Download App" />
+            </a>
                 <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>E-commerce Website</h3>
-              <p>E-commerce Website made with the help of Wordpress and woocommerce. This website is developed for the online shopping.
+              <h3>E-commerce Website + App </h3>
+              <p>E-commerce Website made with the help of Wordpress and woocommerce. This website and app is developed for the online shopping.
               </p>
             </div>
             <footer>
