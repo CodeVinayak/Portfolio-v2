5b44c8be72f5723341ee7df742e15d17f6766e3e
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-08-14 21:07:37 +0530

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -17,17 +17,20 @@ export function Portfolio() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://ecommerce.vinayaksingh.in/wp-content/uploads/2022/07/Ecommerce.apk" target="_blank" rel="noreferrer">
+                <a href="https://madewithluv.in/" target="_blank" rel="noreferrer">
               <img src={DownloadApp} alt="Download App" />
             </a>
-                <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
+                {/* <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
-                </a>
+                </a> */}
               </div>
             </header>
             <div className="body">
-              <h3>E-commerce Website + App </h3>
-              <p>The Website is made with the help of Wordpress and woocommerce. The Android app is made with the help of Java that converted website into app.
+              {/* <h3>E-commerce Website + App </h3> */}
+              <h3>E-commerce Website  </h3>
+              {/* <p>The Website is made with the help of Wordpress and woocommerce. The Android app is made with the help of Java that converted website into app.
+              </p> */}
+              <p>The Website is made with the help of Wordpress and woocommerce. 
               </p>
             </div>
             <footer>
