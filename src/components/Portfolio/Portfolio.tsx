4a73059ab08534a3bcff8b50f6937d2cf4b0a441
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-08-14 21:09:29 +0530

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -18,7 +18,8 @@ export function Portfolio() {
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
                 <a href="https://madewithluv.in/" target="_blank" rel="noreferrer">
-              <img src={DownloadApp} alt="Download App" />
+                <img src={externalLinkIcon} alt="Visitar site" />
+              {/* <img src={DownloadApp} alt="Download App" /> */}
             </a>
                 {/* <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
