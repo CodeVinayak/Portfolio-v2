8561ad4b93e8486beeeb80c9782d49170a6f216e
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-08-21 21:17:31 +0530

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -12,27 +12,49 @@ export function Portfolio() {
 
       <div className="projects">
 
+
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <img src={DownloadApp} alt="Download App" />
+                <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>E-commerce Website + App </h3>
+              <p>The Website is made with the help of Wordpress and woocommerce. The Android app is made with the help of Java that converted website into app.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+                <li>JavaScript</li>
+
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
                 <a href="https://madewithluv.in/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              {/* <img src={DownloadApp} alt="Download App" /> */}
-            </a>
-                {/* <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
-                </a> */}
+                </a>
               </div>
             </header>
             <div className="body">
-              {/* <h3>E-commerce Website + App </h3> */}
-              <h3>E-commerce Website  </h3>
-              {/* <p>The Website is made with the help of Wordpress and woocommerce. The Android app is made with the help of Java that converted website into app.
-              </p> */}
+              <h3>Home Decor Site</h3>
               <p>
-                This E-commerce Website is made to Sell Home Decore Products. 
+                This E-commerce Website is made by Vinayak Singh for my client to Sell Home Decore Products online.
               </p>
             </div>
             <footer>
@@ -50,6 +72,30 @@ export function Portfolio() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
+                <a href="https://justmedicalbooks.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Medical Book</h3>
+              <p>This site is made by vinayaksingh for my client to Sell Medical Book Online to students.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+                <li>Woocommerce</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
                 <a href="https://drrohitdamor.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
                 </a>
@@ -57,7 +103,7 @@ export function Portfolio() {
             </header>
             <div className="body">
               <h3>Online Appointment Booking</h3>
-              <p>Customers Can Schedule an Appointment with You in under a Minute. This website is speically designed for Medical Appointment booking for our client <a href="https://drrohitdamor.vinayaksingh.in/"> DR. ROHIT DAMOR</a>
+              <p>This website is speically designed for Medical Appointment booking for our client <a href="https://drrohitdamor.vinayaksingh.in/"> DR. ROHIT DAMOR</a>Customers Can Schedule an Appointment with You in under a Minute.
               </p>
             </div>
             <footer>
@@ -95,61 +141,6 @@ export function Portfolio() {
           </div>
         </ScrollAnimation>
 
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-              <div className="project-links">
-                {/* <a href="https://github.com/CodeVinayak/Portfolio-v1" target="_blank" rel="noreferrer">
-                  <img src={githubIcon} alt="Visitar site" />
-                </a> */}
-                <a href="https://justmedicalbooks.com/" target="_blank" rel="noreferrer">
-                  <img src={externalLinkIcon} alt="Visitar site" />
-                </a>
-              </div>
-            </header>
-            <div className="body">
-              <h3>Sell Medical Books</h3>
-              <p>I have also made an website to direct distributor of all the major medical books online.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Wordpress</li>
-                <li>Woocommerce</li>
-              
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-              <div className="project-links">
-                {/* <a href="https://github.com/" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="Visitar site" />
-              </a> */}
-                <a href="https://blog.vinayaksingh.in/" target="_blank" rel="noreferrer">
-                  <img src={externalLinkIcon} alt="Visitar site" />
-                </a>
-              </div>
-            </header>
-            <div className="body">
-              <h3>Blog</h3>
-              <p>
-                Blog about Best Online Shopping Deals and Offers. Enjoy Savings , Add Bookmark our website. Never Miss any Loot Deals and Tricks.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Wordpress</li>
-                {/* <li>JavaScript</li> */}
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
@@ -167,7 +158,7 @@ export function Portfolio() {
             <div className="body">
               <h3>Spotify</h3>
               <p>
-              A modern Web-Based music player made using ReactJS. Clean UI with dark mode, High-quality online music streaming with no lag, Ad Free, Download music to your system, Global Music.
+                A modern Web-Based music player made using ReactJS. Clean UI with dark mode, High-quality online music streaming with no lag, Ad Free, Download music to your system, Global Music.
               </p>
             </div>
             <footer>
