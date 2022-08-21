db23d2c8ca3fdcbb860f1240288f84f4cc1b6652
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-08-21 21:25:28 +0530

    minor changes

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -19,7 +19,12 @@ export function Portfolio() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <img src={DownloadApp} alt="Download App" />
+                {/* <a href="https://github.com/" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a> */}
+               <a href="https://github.com/" target="_blank" rel="noreferrer">
+               <img src={DownloadApp} alt="Download App" />
+              </a>
                 <a href="https://ecommerce.vinayaksingh.in/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
                 </a>
@@ -102,8 +107,8 @@ export function Portfolio() {
               </div>
             </header>
             <div className="body">
-              <h3>Online Appointment Booking</h3>
-              <p>This website is speically designed for Medical Appointment booking for our client <a href="https://drrohitdamor.vinayaksingh.in/"> DR. ROHIT DAMOR</a>Customers Can Schedule an Appointment with You in under a Minute.
+              <h3>Dr. Appointment Booking</h3>
+              <p>This website is speically designed for Medical Appointment booking for our client <a href="https://drrohitdamor.vinayaksingh.in/"> DR. ROHIT DAMOR</a>. Customers Can Schedule an Appointment with You in under a Minute.
               </p>
             </div>
             <footer>
--- src/components/Portfolio/styles.ts
+++ src/components/Portfolio/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   margin-top: 15rem;
-
+  
   h2{
     text-align: center;
     font-size: 4rem;
