e870d622f18d2b317fef798f34b660eb1190fa07
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-08-27 17:04:35 +0530

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -22,7 +22,7 @@ export function Hero() {
           <h3>Full Stack Developer</h3>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">4 Year of experience</p>
+          <p className="small-resume">4 Years of experience</p>
         </ScrollAnimation>
 
 
