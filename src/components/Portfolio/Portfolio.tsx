45e4883b746b4e9413744dd54ace4f1b3dc1834d
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-08-27 17:17:22 +0530

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -101,14 +101,14 @@ export function Portfolio() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://drrohitdamor.vinayaksingh.in/" target="_blank" rel="noreferrer">
+                <a href="https://drrohitdamor.com/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
                 </a>
               </div>
             </header>
             <div className="body">
               <h3>Appointment Booking</h3>
-              <p>This website is speically designed for Medical Appointment booking for our client <a href="https://drrohitdamor.vinayaksingh.in/"> DR. ROHIT DAMOR</a>. Customers Can Schedule an Appointment with You in under a Minute.
+              <p>This Medical Appointment booking Website is made by Vinayak Singh for my client to get online appointment. Customers Can Schedule an Appointment with you in under a Minute.
               </p>
             </div>
             <footer>
@@ -128,14 +128,14 @@ export function Portfolio() {
                 {/* <a href="https://github.com/" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="GitHub" />
               </a> */}
-                <a href="https://mitulindustries.vinayaksingh.in/" target="_blank" rel="noreferrer">
+                <a href="https://mitulindustries.com/" target="_blank" rel="noreferrer">
                   <img src={externalLinkIcon} alt="Visitar site" />
                 </a>
               </div>
             </header>
             <div className="body">
               <h3>Manufacturing Industries</h3>
-              <p>Manufacturing Industrial Website for Showing Product and information about their business in online world.
+              <p>This Manufacturing Industrial Website is made by Vinayak Singh for my client to Showchase Product and information about their business in online world.
               </p>
             </div>
             <footer>
