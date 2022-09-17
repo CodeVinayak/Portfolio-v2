2ece4d38f7ca5837b4dec0469d5b0e6edb0081e5
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-09-17 15:49:15 +0530

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
 
-import Curriculo from '../../assets/CV_VinayakSingh.pdf'
+import CV from '../../assets/CV_VinayakSingh_2022.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
 
@@ -46,7 +46,7 @@ export function Header() {
           <NavHashLink smooth to="#contact" onClick={closeMenu}>
             Contact
           </NavHashLink>
-          <a href={Curriculo} download className="button">
+          <a href={CV} download className="button">
             CV
           </a>
         </nav>
