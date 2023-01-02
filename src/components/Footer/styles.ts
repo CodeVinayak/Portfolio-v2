bf1feef64df5edb80aa37a639cb901d2213336c6
Vinayak Singh <vinayaksingh10142@gmail.com>
2023-01-02 10:21:55 +0530

    Update styles.ts

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Container = styled.footer`
   background-color: #2b2b2b;
   padding: 3rem 15rem;
-  margin-top: 15rem;
+  margin-top: 10rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
