dafb8e839a7c32a26a723a4cd124b4804af3aedb
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-01-09 22:12:41 +0530

    cv updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
 
-import CV from '../../assets/CV_VinayakSingh_2022.pdf'
+import CV from '../../assets/CV_VinayakSingh.pdf' 
 export function Header() {
   const [isActive, setActive] = useState(false)
 
