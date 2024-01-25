c15469f63d4972c88ec4b6e3f8ce02c9df83d6a4
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-01-26 01:58:10 +0530

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,8 +20,8 @@ export function Header() {
     <Container className="header-fixed">
       <Router>
         <HashLink smooth to="#home" className="logo">
-          <span>{"< Vinayak "}</span>
-          <span>{" Singh />"}</span>
+          <span>{"<Vinayak "}</span>
+          <span>{" Singh/>"}</span>
         </HashLink>
 
         <input
