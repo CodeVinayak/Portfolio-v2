ab5bf9c7d8984aedb7b04efab796bf9013d34adb
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-04-29 13:11:07 +0530

    Update Project.tsx

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -16,27 +16,19 @@ export function Project() {
       <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-                <a href="https://kameleon.co.in/" target="_blank" rel="noreferrer">
-                  <img src={externalLink} alt="Visit site" />
-                </a>
+                <a href="https://github.com/CodeVinayak/Serverless-Voting-Application" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
                 <a href="https://vote.vinayaksingh.com" target="_blank" rel="noreferrer">
-                  <img src={githubIcon} alt="Visit site" />
-                </a>
-              </div>
+                  <img src={externalLink} alt="Visit site" />
+                </a> </div>
             </header>
             <div className="body">
-              <h3>Kameleon - Cloth Selling Site</h3>
-              <p>
-              This website sells men's cloth products and helps men look their best. It is easy to find what you need, whether it's cloth shirts, pants, or accessories. The site is useful for men who want to look their best and need some help.
-              </p>
+              <h3>Serverless Voting Application for Programming Languages</h3>
+              <p> Developed a serverless voting app using React, AWS Lambda, API Gateway, and DynamoDB, enabling users to view programming languages, cast votes, and access details through an interactive UI, leveraging serverless architecture for scalability and cost-efficiency. </p>
             </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Shopify</li>
-              </ul>
-            </footer>
+            <footer> <ul className="tech-list"> <li>React</li> <li>AWS Lambda</li> <li>API Gateway</li> <li>DynamoDB</li> </ul> </footer>
           </div>
         </ScrollAnimation>
 
