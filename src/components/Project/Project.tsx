22acd37ece07bd362fa4bc536a28b2bf7a44fd45
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-04-29 12:40:30 +0530

    Update Project.tsx

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -154,6 +154,37 @@ export function Project() {
   </div>
 </ScrollAnimation>
 
+<ScrollAnimation animateIn="flipInX">
+  <div className="project">
+    <header>
+      <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+        <title>Code</title>
+        <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+      </svg>
+      <div className="project-links">
+        <a href="https://github.com/CodeVinayak/Real-Time-Hand-Gesture-Driven-3D-Object-Manipulation" target="_blank" rel="noreferrer">
+          <img src={githubIcon} alt="Visit site" />
+        </a>
+        <a href="https://youtu.be/NOm-3MynPLE" target="_blank" rel="noreferrer">
+          <img src={externalLink} alt="Visit site" />
+        </a>
+      </div>
+    </header>
+    <div className="body">
+      <h3>Real-Time Hand Gesture-Driven 3D Object Manipulation</h3>
+      <p>Developed a real-time hand gesture recognition system for intuitive 3D object manipulation. Leveraged a multimodal approach integrating OpenCV for video processing, MediaPipe for hand detection/landmark extraction, and scikit-learn for machine learning model training. Achieved 95.2% accuracy in hand gesture classification with efficient real-time performance.</p>
+    </div>
+    <footer>
+      <ul className="tech-list">
+        <li>OpenCV</li>
+        <li>MediaPipe</li>
+        <li>scikit-learn</li>
+      </ul>
+    </footer>
+  </div>
+</ScrollAnimation>
+
+
         {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
@@ -206,7 +237,7 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
-        <ScrollAnimation animateIn="flipInX">
+        {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
@@ -228,7 +259,7 @@ export function Project() {
               </ul>
             </footer>
           </div>
-        </ScrollAnimation>
+        </ScrollAnimation> */}
 
         {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
