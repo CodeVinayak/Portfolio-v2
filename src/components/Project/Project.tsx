cbf4e934f1e1214124b375f63c12c7d052683f32
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-04-29 12:30:14 +0530

    Update Project.tsx

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -123,7 +123,38 @@ export function Project() {
           </div>
         </ScrollAnimation> */}
 
-        <ScrollAnimation animateIn="flipInX">
+<ScrollAnimation animateIn="flipInX">
+  <div className="project">
+    <header>
+      <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+        <title>Folder</title>
+        <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+      </svg>
+      <div className="project-links">
+        <a href="https://github.com/CodeVinayak/MediBook-Medical-Appointment-Scheduler-with-Health-History" target="_blank" rel="noreferrer">
+          <img src={githubIcon} alt="Visit site" />
+        </a>
+        <a href="https://medibook.vinayaksingh.com" target="_blank" rel="noreferrer">
+          <img src={externalLink} alt="Visit site" />
+        </a>
+      </div>
+    </header>
+    <div className="body">
+      <h3>MediBook: Medical Appointment Scheduler with Health History</h3>
+      <p>Developed a secure medical appointment management platform with CRUD functionality, utilizing role-based access control, encryption techniques, and a robust MySQL database with normalized schema design and stored procedures for efficient scheduling, data privacy, and reliable retrieval of patient records.</p>
+    </div>
+    <footer>
+      <ul className="tech-list">
+        <li>PHP</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>MySQL</li>
+      </ul>
+    </footer>
+  </div>
+</ScrollAnimation>
+
+        {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
@@ -146,7 +177,7 @@ export function Project() {
               </ul>
             </footer>
           </div>
-        </ScrollAnimation>
+        </ScrollAnimation> */}
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
