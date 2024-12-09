4967144dac7516119f8370fed37e6b75a655d901
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-12-09 17:53:08 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
+import { Analytics } from "@vercel/analytics/react"
+
 import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <GlobalStyle></GlobalStyle>
       <Header></Header>
       <Main></Main>
+      <Analytics />
       <Footer></Footer>
     </>
   )
