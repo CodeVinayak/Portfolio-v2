8a8ba06a471637dc8114a7dcf5b38da03cace9be
Vinayak Singh <vinayaksingh10142@gmail.com>
2025-03-31 23:11:03 +0530

    resume updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/Vinayak_Kumar_Singh_Resume.pdf'
+import Resume from '../../assets/Vinayak_Singh_Resume.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
